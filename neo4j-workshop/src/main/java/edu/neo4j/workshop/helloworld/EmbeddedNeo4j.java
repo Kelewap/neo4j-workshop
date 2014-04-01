ba315f259940a45fdf113641e1570db14f694c7d
Michal Partyka <partyks@ubuntu.(none)>
2014-04-01 10:17:02 -0700

    init

---
+++ neo4j-workshop/src/main/java/edu/neo4j/workshop/helloworld/EmbeddedNeo4j.java
@@ -0,0 +1,10 @@
+package edu.neo4j.workshop.helloworld;
+
+/**
+ * @author Michal Partyka
+ */
+public class EmbeddedNeo4j {
+    private static final String DB_PATH = "target/neo4j-hello-db";
+
+
+}
